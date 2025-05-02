db5e31872836c9be9c86fdac3f239e05bcd4236c
migueeldr <miguel.diegor@edu.uah.es>
2025-05-02 12:39:45 +0200

    Inicio de una nueva era

--- Trabajo final/src/main/java/com/example/trabajofinal/HelloController.java
+++ Trabajo final/src/main/java/com/example/trabajofinal/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.trabajofinal;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -11,4 +14,16 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-}
+    @FXML
+    protected void onMiBotonNuevaPartidaClick() {  //onMiBotonNuevaPartidaClick
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("Hello! Ventana hija:");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+}}
